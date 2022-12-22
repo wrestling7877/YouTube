ec6a8c268d83945eebf33dce0925a82736663dd0
Kamoliddin <kamolAbdugafarovv@gmail.com>
2022-12-22 17:29:37 +0500

    shular qoshildi

--- src/main/java/com/example/service/ProfileService.java
+++ src/main/java/com/example/service/ProfileService.java
@@ -1,7 +1,9 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileService {
+
 }
