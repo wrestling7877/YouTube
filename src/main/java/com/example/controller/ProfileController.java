6a042b1dc9ac55bc8dca5c7acd2951b0067d0ae0
Kamoliddin <kamolAbdugafarovv@gmail.com>
2022-12-22 17:35:37 +0500

    shular qoshildi

--- src/main/java/com/example/controller/ProfileController.java
+++ src/main/java/com/example/controller/ProfileController.java
@@ -1,9 +1,19 @@
 package com.example.controller;
 
+import com.example.dto.entityDto.ProfileDto;
+import com.example.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
+    @Autowired
+    private ProfileService profileService;
+    public ResponseEntity<?> create (ProfileDto dto){
+
+        return null;
+    }
 }
