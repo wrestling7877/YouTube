e88880fef27ace753a07a53fad6dd59a3c891e3d
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2022-12-22 20:03:40 +0500

    MailService

---
+++ src/main/java/com/example/service/EmailHistoryService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EmailHistoryService {
+}
--- src/main/java/com/example/service/ResourceBundleService.java
+++ src/main/java/com/example/service/ResourceBundleService.java
@@ -1,17 +1,29 @@
 package com.example.service;
 
 import com.example.enums.Language;
+import com.example.repository.ProfileRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
-
+@Slf4j
 @Service
 public class ResourceBundleService {
 
     @Autowired
     private ResourceBundleMessageSource messageSource;
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Autowired
+    private AttachService attachService;
+    @Autowired
+    private EmailHistoryService emailHistoryService;
+
+    @Autowired
+    private MailService mailService;
+
 
     public String getMessage(String code, String lang) {
         return messageSource.getMessage(code, null, new Locale(lang));
