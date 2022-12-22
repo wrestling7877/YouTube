2c5e16f48e2576261652f4288afcd3aa0bd2c27f
wrestling7877 <wrestling7877@gmail.com>
2022-12-22 14:54:33 +0500

    Project created

---
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.example.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Kun.uz APi documentation", version = "1.0", description = ""))
+public class SwaggerConfig {
+
+}
