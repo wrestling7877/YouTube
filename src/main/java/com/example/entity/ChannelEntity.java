4f3d3ce454913f72316c45dd97ea7abb9fb888ea
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2022-12-22 21:33:35 +0500

    shular

---
+++ src/main/java/com/example/entity/ChannelEntity.java
@@ -0,0 +1,49 @@
+package com.example.entity;
+
+import com.example.enums.ChannelStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "channel")
+public class ChannelEntity {
+    @Id
+    @GeneratedValue(generator = "generator_uuid")
+    @GenericGenerator(name = "generator_uuid",
+            strategy = "org.hibernate.id.UUIDGenerator")
+
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column(name = "photo_id")
+    private String photoId;
+    @ManyToOne
+    @JoinColumn(name = "photo_id", insertable = false, updatable = false)
+    private AttachEntity photo;
+
+    @Column
+    private String description;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column
+    private ChannelStatus status;
+
+    @Column(name = "banner_id")
+    private String bannerId;
+    @ManyToOne
+    @JoinColumn(name = "banner_id", insertable = false, updatable = false)
+    private AttachEntity banner;
+
+    @Column(name = "profile_id")
+    private Integer profileId;
+    @ManyToOne
+    @JoinColumn(name = "profile_id", insertable = false, updatable = false)
+    private ProfileEntity profile;
+
+}
